d13228b07096d3e03e5432123a4b2c9312f04cf7
mayarabezerra <mayarab253@gmail.com>
2021-09-16 17:46:13 -0300

    submitform e correção de rotas

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
-import FormSignup from '../../components/form/formSignup';
-import FormSucess from '../../components/form/formSucess';
+import FormSignup from '../../components/forms/formSignup';
+import FormSucess from '../../components/forms/formSucess';
 import './register.css';
 
 export default function Register() {
   const [isSubmitted, setIsSubmitted] = useState(false);
-
-  function submitForm() {
+  const submitForm = () => {
     setIsSubmitted(true);
-  }
+  };
   return (
     <>
       {!isSubmitted ? <FormSignup submitForm={submitForm} /> : (<FormSucess />)}
